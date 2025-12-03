bd3ee6c5723c11e02f88a509edb17142f48529a2
Gezraf <59087430+Gezraf@users.noreply.github.com>
2025-12-03 11:32:06 +0200

    Implement max diagonal sum and matrix rotation
    Added functions to calculate the maximum diagonal sum and rotate a matrix by 90 degrees.

--- ex3/part2.c
+++ ex3/part2.c
@@ -5,6 +5,7 @@ Author: Guy Shitrit, ID: 330707761
 #include <stdio.h>
 #include <math.h>
 #define N 4
+#define SIZE 5
 
 
 void FindValleys(int mat[N][N]) {
@@ -27,14 +28,64 @@ void FindValleys(int mat[N][N]) {
         printf("No result found");
 }
 
-int main() {
-    int mat[N][N] = {
-        {5,3,4,6},
-        {7,2,8,9},
-        {9,5,1,4},
-        {6,7,3,2}
-    };
 
-    FindValleys(mat);
+int sumDiagonal(int mat[SIZE][SIZE], int i, int j) {
+    int sum = 0;
+    
+    while(i >= 0 && i < SIZE && j >= 0 && j < SIZE) {
+        sum += mat[i][j];
+        i++;
+        j--;
+    }
+
+    return sum;
+}
+
+
+void MaxSumDiagonal(int mat[SIZE][SIZE]) {
+    int max = -1;
+    for(int i = 0; i<SIZE; i++) {
+        for(int j = 0; j<SIZE; j++) {            
+            int sum = sumDiagonal(mat, i, j);
+            if(sum > max) {
+                max = sum;
+            }
+        }
+    }
+    
+    printf("The max sum is %d", max);
+}
+
+
+
+void RotateMatrix90(int mat[SIZE][SIZE]) {
+    int temp[SIZE][SIZE];
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++){
+            temp[i][j] = mat[SIZE - i - 1][j];
+        }
+    }
+
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++){
+            mat[i][j] = temp[i][j];
+        }
+    }
+}
+
+int main()
+{
+    int mat[SIZE][SIZE] = {
+        {5,3,4,6,1},
+        {7,2,8,9,0},
+        {9,5,1,4,1},
+        {6,7,3,2,3},
+        {2,6,1,0,6}
+    };
+    
+    
+    MaxSumDiagonal(mat);
     return 0;
 }
